513a875bf13c9ed766ecfc70cea20ab8f8ac4e69
Albert <al.ferrer.artiaga@gmail.com>
2019-10-13 21:50:52 +0200

    add indexOf

---
+++ staff/albert-ferrer/hooray/original-functions/indexOf.js
@@ -0,0 +1,23 @@
+/**
+ * returns the first index at which a given element can be found in the array
+ *  
+ * @param {Array} array Array with the elements 
+ * @param {argument} element Element to locate in the array.
+ * @returns {Array} Array con los elementos que han coincidido
+ */
+
+
+function indexOf(array, argument){
+    var result = [];
+
+    for (var i = 0; i < array.length;i++){
+        if (element === array[i]){
+            result = array[i];
+
+        };
+    };
+    return result;
+};
+
+
+
