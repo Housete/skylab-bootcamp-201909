5d242bd0964b0d4785b4f16af175cc0436cd858c
Albert <al.ferrer.artiaga@gmail.com>
2019-10-14 15:02:49 +0200

    add changes SpecRunner.html

---
+++ staff/albert-ferrer/hooray/original-spec/concat.spec.js
@@ -0,0 +1,14 @@
+describe('concat',function(){
+    it ('concat', function () {
+        var array1 = ['a','b'];
+        var array2 = ['c','d'];
+        var array = ['a','b','c','d'];
+        expect(concat(array1,array2)).toEquals(array);
+    })
+    it ('concat', function () {
+        var array1=[]
+       
+        expect(()).toBe();
+    
+    
+});
