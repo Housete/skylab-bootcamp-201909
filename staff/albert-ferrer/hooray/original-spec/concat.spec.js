14a299b33467c94c2a2e0a7067dac13da5a8c144
Albert <al.ferrer.artiaga@gmail.com>
2019-10-14 17:59:43 +0200

    add concat spec

--- staff/albert-ferrer/hooray/original-spec/concat.spec.js
+++ staff/albert-ferrer/hooray/original-spec/concat.spec.js
@@ -1,14 +1,16 @@
 describe('concat',function(){
-    it ('concat', function () {
+    it ('Should return an array with the elements of the other arrays.', function () {
         var array1 = ['a','b'];
         var array2 = ['c','d'];
-        var array = ['a','b','c','d'];
-        expect(concat(array1,array2)).toEquals(array);
+        var result = ['a','b','c','d'];
+        expect(concat(array1,array2)).toEquals(result);
     })
-    it ('concat', function () {
-        var array1=[]
+    it ('Should return an empty array.', function () {
+        var array1 = [];
+        var array2 = [];
+        var result = [];
        
-        expect(()).toBe();
-    
-    
-});
+        expect(concat(array1,array2)).toBe(result);
+    })
+
+ }); 
