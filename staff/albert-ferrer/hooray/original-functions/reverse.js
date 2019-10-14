eb56ff3678bc24b36e5f3c1567fef207b05ac396
Albert <al.ferrer.artiaga@gmail.com>
2019-10-14 10:34:12 +0200

    add changes reverse.js

--- staff/albert-ferrer/hooray/original-functions/reverse.js
+++ staff/albert-ferrer/hooray/original-functions/reverse.js
@@ -9,9 +9,10 @@ var numbers = ['one','two','three'];
 
 function reverse(array){
     newAr=[];
+    let j = 0
     for (var i=array.length -1; i >= 0; i--){
-        newAr+= array[i];
+        newAr[j++]= array[i];
     };
-
+    return newAr;
 };
 console.log(reverse(numbers));
